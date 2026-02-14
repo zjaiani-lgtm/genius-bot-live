27d51b122c45d9fe443ac016201180a55ebd8abd
zjaiani-lgtm <z.jaiani@gmail.com>
2026-02-15 01:38:31 +0400

    Delete core/signal_validator.py

--- core/signal_validator.py
+++
@@ -1,9 +0,0 @@
-
-def validate_signal(decision: dict, sell_fw: dict):
-    action = decision.get("FINAL_ACTION", "HOLD")
-
-    if action == "SELL":
-        if sell_fw.get("SELL_DECISION") != "SELL":
-            return False, "SELL_BLOCKED"
-
-    return True, action
